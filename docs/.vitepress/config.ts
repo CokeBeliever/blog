1f39aff54e7b539f5daf4cbbb95f76f311357176
CokeBeliever <cokebeliever@163.com>
2022-10-17 16:41:41 +0800

    初始化项目

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,37 @@
+import { defineConfig } from "vitepress";
+
+export default defineConfig({
+  title: "CokeBeliever 的博客",
+  description: "一个博客网站",
+  lang: "zh-CN",
+  base: "/",
+  themeConfig: {
+    logo: "/logo.svg",
+    nav: [],
+    sidebar: [
+      {
+        text: "算法和数据结构",
+        collapsible: true,
+        collapsed: true,
+        items: [],
+      },
+      {
+        text: "工程化",
+        collapsible: true,
+        collapsed: true,
+        items: [],
+      },
+      {
+        text: "其他",
+        collapsible: true,
+        collapsed: true,
+        items: [],
+      },
+    ],
+    socialLinks: [{ icon: "github", link: "https://github.com/CokeBeliever" }],
+    editLink: {
+      pattern: "https://github.com/CokeBeliever",
+      text: "在 GitHub 编辑此页",
+    },
+  },
+});
